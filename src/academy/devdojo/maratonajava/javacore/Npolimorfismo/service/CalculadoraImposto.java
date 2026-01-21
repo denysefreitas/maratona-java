e5ec4a981aea807738db9b25ae0703375a536de2
Denyse Freitas Santos <denysefreitass@gmail.com>
2026-01-20 23:08:13 -0300

    feat(polimorfismo): aprofundar conceitos de polimorfismo
    - implementar novos cenários de polimorfismo
    - implementar polimorfismo em tempo de execução via sobrescrita
    - implementar validações com instanceof
    - implementar conversão de objetos da superclasse para a subclasse

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/service/CalculadoraImposto.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/service/CalculadoraImposto.java
@@ -1,25 +1,43 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.service;
 
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.domain.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.domain.Produto;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.domain.Tomate;
 
 public class CalculadoraImposto {
     // "Regra de negócio"
-   public static void calculadoraImpostoComputador(Computador computador){
-       double imposto = computador.calcularImposto();
+//   public static void calcularImpostoComputador(Computador computador){
+//       double imposto = computador.calcularImposto();
+//
+//       System.out.println("Relatório de imposto do computador");
+//       System.out.println("Computador: " + computador.getName());
+//       System.out.println("Valor: " + computador.getValue());
+//       System.out.println("Imposto: " + imposto);
+//   }
+//
+//   public static void calcularImpostoTomate(Tomate tomate){
+//       double imposto = tomate.calcularImposto();
+//
+//       System.out.println("Relatório de imposto do tomate");
+//       System.out.println("Tomate: " + tomate.getName());
+//       System.out.println("Valor: " + tomate.getValue());
+//       System.out.println("Imposto: " + imposto);
+//   }
 
-       System.out.println("Relatório de imposto do computador");
-       System.out.println("Computador: " + computador.getName());
-       System.out.println("Valor: " + computador.getValue());
-       System.out.println("Imposto: " + imposto);
-   }
+   public static void calcularImposto(Produto produto){
+       System.out.println("Relatório de imposto");
+       double imposto = produto.calcularImposto();
 
-   public static void calculadoraImpostoTomate(Tomate tomate){
-       double imposto = tomate.calcularImposto();
+       System.out.println("Relatório de imposto do produto");
+       System.out.println("Produto: " + produto.getName());
+       System.out.println("Valor: " + produto.getValue());
+       System.out.println("Imposto a ser pago: " + imposto);
 
-       System.out.println("Relatório de imposto do tomate");
-       System.out.println("Tomate: " + tomate.getName());
-       System.out.println("Valor: " + tomate.getValue());
-       System.out.println("Imposto: " + imposto);
+       // Valida se o produto é uma instância da subclasse
+       if(produto instanceof Tomate){
+           Tomate tomate = (Tomate) produto;
+//           ((Tomate) produto) permite acessar get.DataValidade() diretamente
+           System.out.println(tomate.getDataValidade());
+       }
    }
 }
